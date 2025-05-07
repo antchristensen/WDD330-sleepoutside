9621147bc5f6b3d5047fcb6b97752c6812bd3014
antchristensen <anthony.christensen1988@gmail.com>
2025-05-06 21:09:33 -0600

    cart problem

--- src/js/product.js
+++ src/js/product.js
@@ -4,8 +4,16 @@ import ProductData from "./ProductData.mjs";
 const dataSource = new ProductData("tents");
 
 function addProductToCart(product) {
-  setLocalStorage("so-cart", product);
+  // Get the current cart or initialize a new array
+  let cart = JSON.parse(localStorage.getItem("so-cart")) || [];
+
+  // Add the new product
+  cart.push(product);
+
+  // Save updated cart using your helper
+  setLocalStorage("so-cart", cart);
 }
+
 // add to cart button event handler
 async function addToCartHandler(e) {
   const product = await dataSource.findProductById(e.target.dataset.id);
