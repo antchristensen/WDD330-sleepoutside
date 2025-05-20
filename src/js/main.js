e4368c0b517e5529960bc3611044187dfa60028d
antchristensen <anthony.christensen1988@gmail.com>
2025-05-19 19:47:47 -0600

    Changed cart to use module and dynamic header and footer

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,8 @@
 import ProductList from './ProductList.mjs';
 import ProductData from './ProductData.mjs';
+import { loadHeaderFooter } from "./utils.mjs";
+
+loadHeaderFooter();
 
 const dataSource = new ProductData('tents');
 const listElement = document.querySelector('.product-list');
